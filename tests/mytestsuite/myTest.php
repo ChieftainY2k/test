a2df64b46e59a84a8b50b60af2bd51cafca6516b
ChieftainY2k <ChieftainY2k@gmail.com>
2016-03-09 16:25:26 +0100

    tests

---
+++ tests/mytestsuite/myTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * test the Json service
+ */
+
+use PHPUnit_Framework_TestCase as TestCase;
+
+class MyTest extends TestCase
+{
+
+    /**
+     * setup object
+     */
+    protected function setUp()
+    {
+
+    }
+
+    /**
+     * my fake test
+     *
+     */
+    public function testSomething()
+    {
+        $this->assertEmpty(true);
+    }
+
+
+}
